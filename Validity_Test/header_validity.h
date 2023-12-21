15bf12343871533cbde96c8a874e8abab834a55e
Pavel <patereschenko@gmail.com>
2023-12-21 15:32:00 +0300

    Update header_validity.h
    добавил константы для ё и Ё

--- Validity_Test/header_validity.h
+++ Validity_Test/header_validity.h
@@ -3,6 +3,8 @@ const int number_right_border = 0x39;
 
 const int russian_left_border = 0xc0;
 const int russian_right_border = 0xff;
+const int russian_yo_lowercase = 0xb8;
+const int russian_yo_uppercase = 0xa8;
 
 const int english_lowercase_left_border = 0x61;
 const int english_lowercase_right_border = 0x7a;
@@ -13,4 +15,4 @@ bool check_answer_validity();
 bool check_russian_input();
 bool check_english_input();
 bool check_number_input();
-bool check_special_symbol_input()æ
+bool check_special_symbol_input()Ã¦
