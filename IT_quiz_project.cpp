7720f79d42de917114f688aa7336727afd16d427
Pavel <patereschenko@gmail.com>
2023-12-21 17:20:49 +0300

    исправил ошибку

--- IT_quiz_project.cpp
+++ IT_quiz_project.cpp
@@ -1,19 +1,3 @@
-#include <iostream>
-#include <Windows.h>
-//<определение списка вопросов и правильных ответов>
-#include "question_list.h"
-const int numQuestions = sizeof(questions) / sizeof(questions[0]); //вычисление количества вопросов
-
-using namespace std;
-int main() {
-	//НАЧАЛО
-	//<приветствие пользователя>
-
-	for (int i = 0; i < numQuestions; i++)
-	{
-			
-	//ВЫВОД вопроса
-	//ВВОД ответа#include <iostream>
 #include <Windows.h>
 #include "Validity_Test/header_validity.h"
 //<определение списка вопросов и правильных ответов>
@@ -42,13 +26,6 @@ int main() {
 	}
 	//<подсчёт результатов>
 	
-	//<вывод результата>
-	//КОНЕЦ
-	return 0;
-}
-	//<проверка на валидность> если ответ валидный, добавляем его в массив ответов
-	}
-	//<подсчёт результатов>
 	//<вывод результата>
 	//КОНЕЦ
 	return 0;
