8ed29f5f119dae790281d5e1b389e25ab0c1c8f5
Igor <pigeon99905@gmail.com>
2023-12-26 20:15:33 +0300

    исправил ошибку в фльтрах

--- IT_quiz_project.cpp
+++ IT_quiz_project.cpp
@@ -1,23 +1,11 @@
 #include <Windows.h>
-<<<<<<< HEAD
 #include "header_validity.h"
 #include "menu.h"
 #include "TestOAiP.h"
 #include "TestOPI.h"
 using namespace std;
-
 int main() {
 	setlocale(LC_ALL, "Russian");
-=======
-#include "menu/menu.h"
-#include "TestOAip/TestOAiP.h"
-#include "TestOPI/TestOPI.h"
-#include "Validity_Test/header_validity.h"
-//<определение списка вопросов и правильных ответов>
-
-int main() {
-
->>>>>>> 61c0ddfa06d9ece008e94035fb4edc403f8a377d
 	//НАЧАЛО
 	//<приветствие пользователя>
 	 
@@ -35,11 +23,6 @@ int main() {
 		cout << "Некорректный номер теста!" << endl;
 		break;
 	}
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 61c0ddfa06d9ece008e94035fb4edc403f8a377d
 	//<подсчёт результатов>
 	
 	//<вывод результата>
